46671da1a3e190117f17586f1baf4056761bb2a3
Ivan Grigorev <absolute1405@gmail.com>
2022-10-22 20:30:22 +0300

    Card added

---
+++ Assets/Sources/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private ClickHandler _clickHandler;
+
+    private void Awake()
+    {
+        _clickHandler.Clicked += OnClick;
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("Карточка нажата");
+    }
+}
---
+++ Assets/Sources/ClickHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Collider2D))]
+public class ClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    public event Action Clicked;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Clicked?.Invoke();
+    }
+}
