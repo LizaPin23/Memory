d0a6ac93e241bae08c4a6bb64b8a047080cbb2a9
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2022-10-22 19:45:18 +0300

    FirsCom

---
+++ Assets/Sources/CardConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardConfig", menuName = "CardConfigs")]
+public class CardConfig : ScriptableObject 
+{
+    [SerializeField] private Sprite _photo;
+    [SerializeField] private string _iD;
+
+    public Sprite photo => _photo;
+    public string iD => _iD;
+}
