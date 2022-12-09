4228e448488b041fb8f7913105f92fa6dbce4f74
LizaPin23 <111238411+LizaPin23@users.noreply.github.com>
2022-12-09 18:01:26 +0300

    Animation

---
+++ Assets/Sources/CardAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator _animatorCard;
+    [SerializeField] private string _boolName;
+
+    public void Idle()
+    {
+        _animatorCard.SetBool(_boolName, false);
+    }
+
+    public void Perevorot1()
+    {
+        _animatorCard.SetBool(_boolName, true);
+    }
+
+    public void Perevorot2()
+    {
+        _animatorCard.SetBool(_boolName, false);
+    }
+}
